c8ef8892059366d7967d5c5c769ace4c1f52b051
selffins <selffins>
2025-11-30 18:04:25 +0300

    Clean-up w/ .gitignore

--- .CoqMakefile.d
+++
@@ -1,6 +0,0 @@
-ListsModPerm/olist.vo ListsModPerm/olist.glob ListsModPerm/olist.v.beautified ListsModPerm/olist.required_vo: ListsModPerm/olist.v 
-ListsModPerm/olist.vio: ListsModPerm/olist.v 
-ListsModPerm/olist.vos ListsModPerm/olist.vok ListsModPerm/olist.required_vos: ListsModPerm/olist.v 
-ListsModPerm/mll.vo ListsModPerm/mll.glob ListsModPerm/mll.v.beautified ListsModPerm/mll.required_vo: ListsModPerm/mll.v ListsModPerm/olist.vo
-ListsModPerm/mll.vio: ListsModPerm/mll.v ListsModPerm/olist.vio
-ListsModPerm/mll.vos ListsModPerm/mll.vok ListsModPerm/mll.required_vos: ListsModPerm/mll.v ListsModPerm/olist.vos
